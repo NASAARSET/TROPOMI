8cca2c5c25c27ee5ae4c96eef0a3ab65b32e9317
NASAARSET <69639909+NASAARSET@users.noreply.github.com>
2022-03-24 06:58:52 -0500

    Add files via upload

---
+++ testcode.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python      
+from netCDF4 import Dataset
+import numpy as np
+from numpy import unravel_index
+import sys
+import time
+import calendar
+import datetime as dt
+import pandas as pd
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
